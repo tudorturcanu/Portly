ffa0931b30ad5120affb885cc5a8dbfea8e7d4de
Tudor Turcanu <tudor.turcanu@scopeworker.com>
2026-07-13 09:34:55 +0200

    Initial Commit

---
+++ Idea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Idea
+//
+//  Created by Tudor Turcanu on 13.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Idea/IdeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaApp.swift
+//  Idea
+//
+//  Created by Tudor Turcanu on 13.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct IdeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
